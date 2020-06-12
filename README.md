dcee246422e6a58ba806f461574fb6245e8bbc67
marksie1988 <marksie1988@users.noreply.github.com>
2020-06-12 23:44:44 +0100

    Fixes

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
-# Installation
-### Download and change folder
+## Requirements
+
+- gcc
+- ncurses-devel
+- zsh 5.8
+
+```
+sudo yum install -y gcc ncurses-devel
+```
+
+```
+#Install Zsh
+wget https://sourceforge.net/projects/zsh/files/zsh/5.8/zsh-5.8.tar.xz/download
+tar -xvJf download
+cd zsh-5.8/
+./configure
+make && sudo make install
+rm -rf download zsh-5.8
+```
+
+## Installation
+
 ```
 cd ~
 git clone this repo
--- installer.sh
+++ installer.sh
@@ -1,15 +1,6 @@
 #!/bin/sh
 path=$(pwd)
 
-#Install Zsh
-wget -O zsh.tar.xz https://sourceforge.net/projects/zsh/files/latest/download
-tar xf zsh.tar.xz
-cd zsh
-./configure
-make install
-cd ..
-rm -rf zsh && rm -rf zsh.tar.xz
-
 #Install spaceship theme
 [ ! -d $path/spaceship-prompt/ ] && git clone https://github.com/denysdovhan/spaceship-prompt.git
 [ ! -d /usr/local/share/zsh/site-functions/ ] && mkdir -p /usr/local/share/zsh/site-functions/
