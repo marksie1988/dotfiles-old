b0f5af2a5931705ba7ee578301f4ef7794dffe29
Steven Marks <steven.marks@rackspace.com>
2020-06-15 19:27:44 +0100

    fix git status

--- zsh/prompt.sh
+++ zsh/prompt.sh
@@ -7,7 +7,7 @@ setopt PROMPT_SUBST
 set_prompt() {
 
 	# Path: http://stevelosh.com/blog/2010/02/my-extravagant-zsh-prompt/
-	PS1+="%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
+	PS1="%{$fg_bold[blue]%}${PWD/#$HOME/~}%{$reset_color%}"
 
 	# Status Code
 	PS1+='%(?.., %{$fg[red]%}%?%{$reset_color%})'
