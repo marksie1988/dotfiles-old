651fd298e69325b0cc62e9d69b70078842dba243
marksie1988 <marksie1988@DESKTOP-JADKVKA.localdomain>
2020-06-24 19:45:41 +0100

    :star: add first aliases

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -2,6 +2,9 @@ alias nrb='npm run build'
 alias sha256='shasum -a 256'
 
 # Git Aliases
-alias gs='git status'
+alias gs='git status -s'
 alias gf='git fetch --prune'
 alias gpm='git pull origin master'
+alias gcb='git checkout -b'
+alias gcm='git add -A && git commit -m'
+alias undo='git reset HEAD~1 --mixed'
